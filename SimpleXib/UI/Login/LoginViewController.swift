eaa80bb4c3804db64fa9568d898a42a6609c22e4
Irfan Pertadima <pertadimairfan@gmail.com>
2019-12-02 15:19:57 +0700

    [irfan] init project

---
+++ SimpleXib/UI/Login/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  SimpleXib
+//
+//  Created by Irfan Pertadima on 12/2/19.
+//  Copyright © 2019 Irfan Pertadima. All rights reserved.
+//
+
+import UIKit
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SimpleXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleXib
+//
+//  Created by Irfan Pertadima on 12/2/19.
+//  Copyright © 2019 Irfan Pertadima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
